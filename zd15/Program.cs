a49dc5f34145b7e8f2ae1043d79009c346555dbe
igor <igor.pravdin163@gmail.com>
2022-09-03 21:56:14 +0400

    задача 15

---
+++ zd15/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.WriteLine("Введите день недели (от 1 до 7)");
+int num = int.Parse(Console.ReadLine());
+
+if ((num == 6) | (num ==7))
+{
+Console.WriteLine("ДА");
+}
+else Console.WriteLine("НЕТ");
