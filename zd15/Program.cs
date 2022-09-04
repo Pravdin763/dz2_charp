c615dbf991c18cb180e64a43184164411ec8dfc2
igor <igor.pravdin163@gmail.com>
2022-09-04 12:26:34 +0400

    формат

--- zd15/Program.cs
+++ zd15/Program.cs
@@ -1,8 +1,8 @@
 ﻿Console.WriteLine("Введите день недели (от 1 до 7)");
 int num = int.Parse(Console.ReadLine());
 
-if ((num == 6) | (num ==7))
+if ((num == 6) | (num == 7))
 {
-Console.WriteLine("ДА");
+    Console.WriteLine("ДА");
 }
 else Console.WriteLine("НЕТ");
