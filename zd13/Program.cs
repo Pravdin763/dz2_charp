e1311a06045a49cbd04b963ad93a2f354c79823f
igor <igor.pravdin163@gmail.com>
2022-09-04 12:21:10 +0400

    задача 13

---
+++ zd13/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("Введите число");
+string num = Console.ReadLine();
+int len = num.Length;
+
+if (len > 2)
+{
+Console.WriteLine(num[2]);
+}
+else  Console.WriteLine("третей цифры нет");
+
